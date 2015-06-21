df68414007df1500791d1dc33dd46778f192d3a9
HyunJae <kimhun456@gmail.com>
2015-06-22 00:00:21 +0900

    post and get

---
+++ html/IO/input.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> input php</title>
+</head>
+<body>
+	<?php
+		/*http://127.0.0.1/IO/input.php?name=kiasdf 
+		address						parameter 
+		*/
+		echo $_GET['name']," sir Welcome!";
+	?>
+</body>
+</html>
